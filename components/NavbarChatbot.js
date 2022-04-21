f4a212c4cd9cf3d3746766ca63863c5591ca3063
Mochamad Rafli Ramadhan <mochamadrafli2018@mail.ugm.ac.id>
2022-04-21 08:15:16 +0700

    Update Navbar Chatbot and .igitignore

--- components/NavbarChatbot.js
+++ components/NavbarChatbot.js
@@ -44,7 +44,7 @@ export default function Navbar() {
             <Link href={url} key={index}>
               <a className="font-bold hover:bg-blue-100 no-underline roboto rounded-lg my-0 px-3 py-2 text-gray-900 hover:text-gray-900">
                 {title}
-                </a>
+              </a>
             </Link>
           ))}
         </>
@@ -52,7 +52,7 @@ export default function Navbar() {
       {tokenEmpty === false && (
         <>
           {[
-            ['Home', '/dashboard/[id]'],
+            ['Home', `/dashboard/${localStorage.getItem('user_id')}`],
             ['Panduan','/guide'],
             ['UAT', '/uat'],
           ].map(([title, url], index) => (
