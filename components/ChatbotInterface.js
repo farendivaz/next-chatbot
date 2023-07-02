bbc1377c4075cb9897b1349ba6e55597fe1420bc
Farendivaz <dandyfariz9@gmail.com>
2023-07-02 15:25:43 +0700

    Initial Project

--- components/ChatbotInterface.js
+++ components/ChatbotInterface.js
@@ -1,19 +1,18 @@
-import React, {useEffect, useRef} from 'react';
-import SendIcon from '@mui/icons-material/Send';
+import React, { useEffect, useRef } from "react";
+// import SendIcon from '@mui/icons-material/Send';
 
 export default function ChatbotInterface({
-    chatInput,
-    handleChange,
-    handleEnter,
-    handleSubmit,
-    handleNextScreening,
-    handleStartScreening,
-    handleYes,
-    handleNo,
-    messageLog
-  }) {
-
-  // create a ref   
+  chatInput,
+  handleChange,
+  handleEnter,
+  handleSubmit,
+  handleNextScreening,
+  handleStartScreening,
+  handleYes,
+  handleNo,
+  messageLog,
+}) {
+  // create a ref
   const messagesEndRef = useRef(null); // empty chat
 
   const scrollToBottom = () => {
@@ -23,118 +22,121 @@ export default function ChatbotInterface({
     clientHeight: amount of container a user sees.
     */
     if (messagesEndRef) {
-      messagesEndRef.current.addEventListener('DOMNodeInserted', event => {
+      messagesEndRef.current.addEventListener("DOMNodeInserted", (event) => {
         const { currentTarget: target } = event;
-        target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
+        target.scroll({ top: target.scrollHeight, behavior: "smooth" });
       });
     }
   };
 
   useEffect(() => {
-    scrollToBottom()
-  }, [messageLog])
+    scrollToBottom();
+  }, [messageLog]);
 
   return (
-    <div className='border-2 border-blue-600 max-w-3xl rounded-xl shadow-lg w-full'>
-      <div className='mx-0 mb-0 w-100 rounded'>
-        <h4 className='bg-blue-600 mb-0 p-1 w-100 rounded-top text-center text-light text-xl'>
+    <div className="border-2 border-blue-600 max-w-3xl rounded-xl shadow-lg w-full">
+      <div className="mx-0 mb-0 w-100 rounded">
+        <h4 className="bg-blue-600 mb-0 p-1 w-100 rounded-top text-center text-light text-xl">
           Chat Bot Sistem Pakar
         </h4>
-        <div 
-          id='messages' 
-          className='bg-chat h-96 max-h-96 mx-0 px-2 py-1 w-100 overflow-y-scroll'
+        <div
+          id="messages"
+          className="bg-chat h-96 max-h-96 mx-0 px-2 py-1 w-100 overflow-y-scroll"
           ref={messagesEndRef}
         >
           {messageLog.map((item, index) => {
             // user chat
-            if (item.sender === 'user') {
+            if (item.sender === "user") {
               return (
-                <p key={index} className='bg-blue-600 border-2 border-white my-1 ml-auto px-3 py-2 max-w-7xl rounded-l-3xl rounded-br-2xl text-light w-min'>
+                <p
+                  key={index}
+                  className="bg-blue-600 border-2 border-white my-1 ml-auto px-3 py-2 max-w-7xl rounded-l-3xl rounded-br-2xl text-light w-min"
+                >
                   {item.message}
                 </p>
-              )
+              );
             }
             // bot chat
-            else if (item.sender === 'bot') {
+            else if (item.sender === "bot") {
               return (
                 <div key={index}>
-                  <i className='inline-block bi bi-robot bg-slate-100 border-2 border-blue-700 px-2 py-1 rounded-full text-dark text-xl'></i>
-                  <p className='inline-block bg-slate-100 border-2 border-blue-700 ml-1 px-2 py-1 rounded-r-3xl rounded-tl-2xl text-dark w-4/5'>
+                  <i className="inline-block bi bi-robot bg-slate-100 border-2 border-blue-700 px-2 py-1 rounded-full text-dark text-xl"></i>
+                  <p className="inline-block bg-slate-100 border-2 border-blue-700 ml-1 px-2 py-1 rounded-r-3xl rounded-tl-2xl text-dark w-4/5">
                     {item.message}
                   </p>
                 </div>
-              )
+              );
             }
           })}
           {/*Chat will be append in here*/}
         </div>
-        <div className='flex p-1 w-100'>
-          <input 
-            id='chat-input' 
-            type='text'
-            className='border-2 border-sky-500 hover:border-blue-600 ml-auto px-3 py-2 rounded-full w-4/5'
+        <div className="flex p-1 w-100">
+          <input
+            id="chat-input"
+            type="text"
+            className="border-2 border-sky-500 hover:border-blue-600 ml-auto px-3 py-2 rounded-full w-4/5"
             value={chatInput}
-            placeholder='Ketik respon kamu ...'
+            placeholder="Ketik respon kamu ..."
             onChange={handleChange}
             onKeyPress={handleEnter}
           />
-          <button 
-            id='btn-submit'
-            type='submit' 
-            value='Submit' 
+          <button
+            id="btn-submit"
+            type="submit"
+            value="Submit"
             onClick={handleSubmit}
-            className='bg-blue-600 hover:bg-blue-800 mx-1 py-2 rounded-full w-1/5'
+            className="bg-blue-600 hover:bg-blue-800 mx-1 py-2 rounded-full w-1/5"
           >
-            <span className="mx-auto text-white text-lg"><SendIcon/></span>
+            <span className="mx-auto text-white text-lg">Send</span>
           </button>
         </div>
       </div>
-      <div className='flex'>
-        <div className='d-grid gap-2 p-1 w-1/3'>
-          <button 
-            id='btn-start'
-            type='submit' 
-            value='Submit' 
-            onClick={handleStartScreening} 
-            className='bg-blue-600 hover:bg-blue-800 py-2 rounded-full text-white'
+      <div className="flex">
+        <div className="d-grid gap-2 p-1 w-1/3">
+          <button
+            id="btn-start"
+            type="submit"
+            value="Submit"
+            onClick={handleStartScreening}
+            className="bg-blue-600 hover:bg-blue-800 py-2 rounded-full text-white"
           >
             Skrining
           </button>
         </div>
-        <div className='d-grid gap-2 p-1 w-1/3'>
-          <button 
-            id='btn-next'
-            type='submit' 
-            value='Submit' 
-            onClick={handleNextScreening} 
-            className='bg-blue-600 hover:bg-blue-800 py-2 rounded-full text-white'
+        <div className="d-grid gap-2 p-1 w-1/3">
+          <button
+            id="btn-next"
+            type="submit"
+            value="Submit"
+            onClick={handleNextScreening}
+            className="bg-blue-600 hover:bg-blue-800 py-2 rounded-full text-white"
           >
             Lanjut
           </button>
         </div>
-        <div className='d-grid gap-2 p-1 w-1/3'>
-          <button 
-            id='btn-yes'
-            type='submit' 
-            value='Submit' 
-            onClick={handleYes} 
-            className='bg-blue-600 hover:bg-blue-800 py-2 rounded-full text-white'
+        <div className="d-grid gap-2 p-1 w-1/3">
+          <button
+            id="btn-yes"
+            type="submit"
+            value="Submit"
+            onClick={handleYes}
+            className="bg-blue-600 hover:bg-blue-800 py-2 rounded-full text-white"
           >
             Ya
           </button>
         </div>
-        <div className='d-grid gap-2 p-1 w-1/3'>
+        <div className="d-grid gap-2 p-1 w-1/3">
           <button
-            id='btn-no'
-            type='submit' 
-            value='Submit' 
-            onClick={handleNo} 
-            className='bg-blue-600 hover:bg-blue-800 py-2 rounded-full text-white'
+            id="btn-no"
+            type="submit"
+            value="Submit"
+            onClick={handleNo}
+            className="bg-blue-600 hover:bg-blue-800 py-2 rounded-full text-white"
           >
             Tidak
           </button>
-        </div> 
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- pages/dashboard/[id].js
+++ pages/dashboard/[id].js
@@ -1,79 +1,82 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import {useRouter} from 'next/router';
-import Navbar from '../../components/NavbarChatbot';
-import Footer from '../../components/Footer';
-import HeadElement from '../../components/Head';
-import InferenceMachine from '../../components/InferenceMachine';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import Navbar from "../../components/NavbarChatbot";
+import Footer from "../../components/Footer";
+import HeadElement from "../../components/Head";
+import InferenceMachine from "../../components/InferenceMachine";
 
 export default function Crud() {
   const router = useRouter();
   useEffect(() => {
     //get token from local storage when browser reload
-    const token = localStorage.getItem('token');
-    if(!token) { 
-      router.push('/login'); 
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
     }
     getUserData(token);
   }, []);
 
   // authorization
-  const [user, setUser] = useState('');
-  const [errorGetUserData, setErrorGetUserData] = useState('');
+  const [user, setUser] = useState("");
+  const [errorGetUserData, setErrorGetUserData] = useState("");
   const getUserData = async (token) => {
     //set axios header with type Authorization + Bearer token
-    axios.defaults.headers.common['authorization'] = `Bearer ${token}`
-    await axios.get('https://express-mongoose-validator.herokuapp.com/api/verify')
-    .then((response) => {
-      setUser(response.data);
-    })
-    .catch((error) => { 
-      setErrorGetUserData(error.message);
-    })
-  }
+    axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
+    await axios
+      .get("https://express-mongoose-validator.herokuapp.com/api/verify")
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        setErrorGetUserData(error.message);
+      });
+  };
 
   // edit data by id
   const [successEditData, setSuccessEditData] = useState(false);
-  const [errorEditData, setErrorEditData] = useState('');
+  const [errorEditData, setErrorEditData] = useState("");
   const editData = (id) => {
-    axios.put(`http://localhost:5000/api/${id}`)
-    .then(() => {
-      setSuccessEditData(true);
-      window.location.reload(); // refresh web app
-    })
-    .catch((err) => { 
-      setSuccessEditData(false);
-      setErrorEditData(err.message);
-    })
-  }
+    axios
+      .put(`http://localhost:5000/api/${id}`)
+      .then(() => {
+        setSuccessEditData(true);
+        window.location.reload(); // refresh web app
+      })
+      .catch((err) => {
+        setSuccessEditData(false);
+        setErrorEditData(err.message);
+      });
+  };
 
   // delete data by id
   const [successDeleteData, setSuccessDeleteData] = useState(false);
-  const [errorDeleteData, setErrorDeleteData] = useState('');
+  const [errorDeleteData, setErrorDeleteData] = useState("");
   const deleteData = (id) => {
-    axios.delete(`http://localhost:5000/api/${id}`)
-    .then(() => {
-      setSuccessDeleteData(true);
-      // refresh web app
-      window.location.reload();
-    })
-    .catch((err) => { 
-      setSuccessDeleteData(false);
-      setErrorDeleteData(err.message);
-    })
-  }
+    axios
+      .delete(`http://localhost:5000/api/${id}`)
+      .then(() => {
+        setSuccessDeleteData(true);
+        // refresh web app
+        window.location.reload();
+      })
+      .catch((err) => {
+        setSuccessDeleteData(false);
+        setErrorDeleteData(err.message);
+      });
+  };
 
   return (
-    <div className='lg:h-full md:h-screen h-full bg-blue-300 m-0 opensans'>
-      <HeadElement text={`EyeScreening - Dashboard`}/>
-      <div className='bg-blue-500 roboto text-center text-white w-full'>
+    <div className="lg:h-full md:h-screen h-full bg-blue-300 m-0 opensans">
+      <HeadElement text={`EyeScreening - Dashboard`} />
+      <div className="bg-blue-500 roboto text-center text-white w-full">
         Hallo, {user.name}
       </div>
-      <Navbar/>
-      <main className='flex flex-row justify-center mt-3 mb-3'>
-        <InferenceMachine/>
+      <Navbar />
+      <main className="flex flex-row justify-center mt-3 mb-3">
+        <InferenceMachine />
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
